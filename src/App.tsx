d5f0b192a53eb7c3a64b0d81c8fddfe77d247500
henrique <henrique.florencio307@gmail.com>
2024-05-18 20:16:51 -0300

    iniciaizando repositório

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <span className="text-3xl uppercase bold">teste</span>
+    </div>
+  );
+}
+
+export default App;
