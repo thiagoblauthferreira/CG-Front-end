fd0fd452f251b0a0b2c2c3c554052309e46c66d6
Vinicius Gabriel <vinigpo@gmail.com>
2024-05-26 19:20:09 -0300

    Adiciona confirmação ssl

---
+++ src/PkiValidationFile.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function PkiValidationFile() {
+  // Aqui você pode adicionar qualquer lógica necessária para manipular a solicitação dessa rota, 
+  // como enviar o arquivo como uma resposta ou fazer qualquer outra coisa que desejar.
+  return (
+    <div>
+      {/* Aqui você pode adicionar qualquer conteúdo que desejar */}
+      <p>Conteúdo do arquivo de validação do pki...</p>
+    </div>
+  );
+}
+
+export default PkiValidationFile;
