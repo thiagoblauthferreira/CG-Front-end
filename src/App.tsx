b02e8f6652d1c38579952266fecda684975fd32e
henrique <henrique.florencio307@gmail.com>
2024-06-08 22:44:14 -0300

    landing temporária

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import LoginPointScreen from "./pages/Login/login";
 import SignUpScreen from "./pages/Sign Up/doador.signup";
+import { LandingTemporaria } from "./pages/LandingTemporaria/LandingTemporaria";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route element={<LoginPointScreen />} path="/login"></Route>
+        <Route element={<LandingTemporaria />} path="/"></Route>
         <Route element={<SignUpScreen />} path="/cadastro"></Route>
       </Routes>
     </Router>
