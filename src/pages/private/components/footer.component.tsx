219af0c06337ce9384571399b3beb6b273e6f21b
henrique <henrique.florencio307@gmail.com>
2024-06-22 14:55:49 -0300

    dividindo dependencias melhor entre componentes

---
+++ src/pages/private/components/footer.component.tsx
@@ -0,0 +1,26 @@
+export function FooterComponent() {
+  return (
+    <footer className="footer p-10 bg-neutral text-neutral-content">
+      <nav>
+        <h6 className="footer-title">Services</h6>
+        <a className="link link-hover">Branding</a>
+        <a className="link link-hover">Design</a>
+        <a className="link link-hover">Marketing</a>
+        <a className="link link-hover">Advertisement</a>
+      </nav>
+      <nav>
+        <h6 className="footer-title">Company</h6>
+        <a className="link link-hover">About us</a>
+        <a className="link link-hover">Contact</a>
+        <a className="link link-hover">Jobs</a>
+        <a className="link link-hover">Press kit</a>
+      </nav>
+      <nav>
+        <h6 className="footer-title">Legal</h6>
+        <a className="link link-hover">Terms of use</a>
+        <a className="link link-hover">Privacy policy</a>
+        <a className="link link-hover">Cookie policy</a>
+      </nav>
+    </footer>
+  );
+}
---
+++ src/pages/private/homeDoador/components/card.component.tsx
@@ -0,0 +1,39 @@
+export function CardComponent({ index }: { index: number }) {
+  const necessidade = {
+    titulo: "lugar",
+    foto: "",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur aspernatur aut veniam soluta autem tempora dicta quia ipsum reiciendis. Ad consectetur nostrum ratione reprehenderit ipsam exercitationem reiciendis similique esse enim.",
+    lista: ["feijão", "arroz"],
+  };
+
+  return (
+    <div
+      className="max-w-xs bg-base-200 flex flex-col gap-3 mx-auto text-center"
+      key={`card-${index}`}
+    >
+      <div className="aspect-video z-0 relative">
+        <div className="absolute h-full w-full backdrop-grayscale-[.45] z-10"></div>
+        <img
+          className="z-0 w-full h-full object-fit object-center"
+          src={necessidade.foto}
+          alt={necessidade.titulo}
+        />
+      </div>
+      <div className="infos-container p-4 pt-0">
+        <h1 className="font-bold uppercase text-lg">{necessidade.titulo}</h1>
+        <article className="px-3 line-clamp-4 text-justify opacity-75">
+          {necessidade.desc}
+        </article>
+        <ul className="flex px-3 gap-2 pt-3">
+          {necessidade.lista.map((item, i) => {
+            return (
+              <li className="p-2 px-3 bg-primary rounded-full" key={i}>
+                {item}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
