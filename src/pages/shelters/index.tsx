6e1ea02a119fc75683a2d44e37fed77999891d74
gabriel <gabrielalencar33564@gmail.com>
2024-07-20 03:27:47 -0300

    feat: create page shelters

---
+++ src/pages/shelters/index.tsx
@@ -0,0 +1,109 @@
+import { CardPrimary } from "../../components/cards/CardPrimary";
+import { Button, Select } from "../../components/common";
+
+export default function SheltersScreen() {
+  return (
+    <div className="py-8">
+      <div className="my-5">
+        <p className="font-semibold">Filtrar por</p>
+        <div
+          className={`
+            grid grid-cols-1 gap-3
+            sm:grid-cols-2 xl:grid-cols-4
+          `}
+        >
+          <Select
+            options={[
+              { label: "teste", value: "teste" },
+              { label: "teste", value: "teste" },
+            ]}
+          />
+          <Select
+            options={[
+              { label: "teste", value: "teste" },
+              { label: "teste", value: "teste" },
+            ]}
+          />
+          <Select
+            options={[
+              { label: "teste", value: "teste" },
+              { label: "teste", value: "teste" },
+            ]}
+          />
+
+          <div
+            className={`
+              flex justify-end
+            `}
+          >
+            <Button
+              text="Filtrar"
+              className={`
+                bg-black text-white w-full
+              `}
+            />
+          </div>
+        </div>
+      </div>
+
+      <div
+        className={`
+          grid grid-cols-1 gap-3
+          sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4
+        `}
+      >
+        <CardPrimary image="" title="Teste">
+          <div>
+            <p>teste</p>
+            <p>teste, teste, teste</p>
+          </div>
+
+          <div
+            className={`
+              absolute bottom-0 right-0
+              m-4 bg-slate-300 rounded-md px-2
+              cursor-pointer 
+            `}
+          >
+            teste
+          </div>
+        </CardPrimary>
+        <CardPrimary image="" title="Teste">
+          <div>
+            <p>teste</p>
+          </div>
+        </CardPrimary>
+        <CardPrimary image="" title="Teste">
+          <div>
+            <p>teste</p>
+          </div>
+        </CardPrimary>
+        <CardPrimary image="" title="Teste">
+          <div>
+            <p>teste</p>
+          </div>
+        </CardPrimary>
+        <CardPrimary image="" title="Teste">
+          <div>
+            <p>teste</p>
+          </div>
+        </CardPrimary>
+        <CardPrimary image="" title="Teste">
+          <div>
+            <p>teste</p>
+          </div>
+        </CardPrimary>
+        <CardPrimary image="" title="Teste">
+          <div>
+            <p>teste</p>
+          </div>
+        </CardPrimary>
+        <CardPrimary image="" title="Teste">
+          <div>
+            <p>teste</p>
+          </div>
+        </CardPrimary>
+      </div>
+    </div>
+  );
+}
