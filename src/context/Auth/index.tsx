a1b5be857270113892b4cc7a56299737e538b636
gabriel <gabrielalencar33564@gmail.com>
2024-07-22 11:50:38 -0300

    feat: request user with token

--- src/context/Auth/index.tsx
+++ src/context/Auth/index.tsx
@@ -1,11 +1,30 @@
 import React from "react";
 import { IAuthProvider, IContextProvider } from "./interface";
 import { IUser } from "../../interfaces/user";
+import { getUser } from "../../services/user.service";
+import { getCookie } from "../../services/cookie.service";
 
 const AuthContext = React.createContext<IAuthProvider>({} as IAuthProvider);
 
-export const AuthProvider = ({ children }: IContextProvider) => {
+export function AuthProvider({ children }: IContextProvider) {
   const [currentUser, setCurrentUser] = React.useState<IUser | null>(null);
+
+  const loadUser = async () => {
+    try {
+      const token = getCookie("token")
+      if(!token) return 
+      
+      const resp = await getUser()
+      console.log(resp, "user")
+      setCurrentUser(resp.data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  React.useEffect(() => {
+    loadUser()
+  },[])
 
   return <AuthContext.Provider value={{ currentUser }}>{children}</AuthContext.Provider>;
 };
